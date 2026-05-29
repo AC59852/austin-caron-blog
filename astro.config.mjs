df7a7c2ea1b4d9d50cc87240796048d25e9900a4
Austin Caron <austin.caron1@gmail.com>
2026-05-29 03:45:38 -0400

    adding sitemap support

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,16 +9,18 @@ import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://blog.austincaron.com',
   integrations: [vue(), sanity({
     projectId: '0p745kum',
     dataset: 'production',
     apiVersion: '2026-03-01',
     studioBasePath: '/admin',
     useCdn: false
-  }), 
-  react()],
+  }), react(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
